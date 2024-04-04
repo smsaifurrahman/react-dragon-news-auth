28372abaabf24ec6c58e792f6478f341b78d4e01
smsaifurrahman <smsaifur525@gmail.com>
2024-04-04 09:43:55 +0200

    New portal website started

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            this is login
+        </div>
+    );
+};
+
+export default Login;
